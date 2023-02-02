efa5384fe8393199e2f9f3a2c4e1c1cb33a30a3b
kannan <jishnukumbadam@gmail.com>
2023-02-02 18:22:19 +0530

    created model

---
+++ back/Routes/userRoutes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router()
+const User = require('../models/User')
+
+// creating user
+router.post('/', async (req, res) => {
+    try {
+        const { name, email, password, picture } = req.body
+        console.log(req.body);
+        const user= await User.create({name,email,password,picture})
+        res.status(201).json(user)
+    } catch (error) {
+        let msg
+        if (error.code == 11000) {
+            msg="User already exist"
+        }else{
+            msg=e.message
+        }
+        console.log(error);
+        res.status(400).json(msg)
+    }
+}) 
+
+// Login user
+router.post('/login',async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+})
